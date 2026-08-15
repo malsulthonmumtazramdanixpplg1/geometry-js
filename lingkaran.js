4e34eb04bab65e8a2048e95250dd9a2a9cdf7eb0
malsulthonmumtazramdanixpplg1 <m.alsulthonmumtazramdani.xpplg1@gmail.com>
2026-08-15 10:32:39 +0700

    lingkaran

--- lingkaran.js
+++ lingkaran.js
@@ -4,3 +4,9 @@ console.log("KALKULATOR LINGKARAN")
 console.log("====================")
 let r = Number(prompt("Jari-jari : "))
 let d = Number(prompt("Diameter : "))
+let phi = Number(prompt("Phi : "))
+let luas = phi * r * r 
+let keliling = 2 * phi * r
+console.log(`
+    luas = ${luas} cm2
+    keliling = ${keliling} cm`)
