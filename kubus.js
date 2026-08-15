6db0618be3d3efcd78fd2d4c39aa53e4c8a0ab31
malsulthonmumtazramdanixpplg1 <m.alsulthonmumtazramdani.xpplg1@gmail.com>
2026-08-15 21:26:36 +0700

    kubus.js

--- kubus.js
+++ kubus.js
@@ -1,7 +1,7 @@
 const prompt = require("prompt-sync") ({ sigint: true})
-console.log("========================")
+console.log("================")
 console.log("KALKULATOR KUBUS")
-console.log("========================")
+console.log("================")
 let s = Number(prompt("Sisi : "))
 let luas = 6 * s * s 
 let keliling = 12 * s
