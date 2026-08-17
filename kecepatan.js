07146b6c11cf3860263ea24e8dd5e1b9dad5023c
malsulthonmumtazramdanixpplg1 <m.alsulthonmumtazramdani.xpplg1@gmail.com>
2026-08-17 16:10:32 +0700

    bola, kerucut

---
+++ kecepatan.js
@@ -0,0 +1,9 @@
+const prompt = require("prompt-sync") ({ sigint: true})
+console.log("===================")
+console.log(KALKULATOR KECEPATAN)
+console.log("===================")
+let s  = Number(prompt("Jarak tempuh : "))
+let t = Number(prompt("Waktu tempuh : "))
+let kecepatan = s/t
+console.log (`
+    kecepatan = ${kecepatan} km/jam`) 
